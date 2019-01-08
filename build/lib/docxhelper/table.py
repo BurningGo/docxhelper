6881d59af7b2129750bcbd96b79c3bfae0208e17
burninggo <kojae1978@gmail.com>
2019-01-08 16:03:40 +0100

    Version update to 0.11

--- build/lib/docxhelper/table.py
+++ build/lib/docxhelper/table.py
@@ -1,4 +1,8 @@
 from docx import Document
+from docx.oxml import OxmlElement
+from docx.shared import Pt, Inches
+from docx.oxml.ns import qn
+from docx.enum.text import WD_COLOR_INDEX
 
 def AlignAllCellsVerticallyCenter(table):
     n_row = len(table.rows)
@@ -11,4 +15,17 @@ def AlignAllCellsVerticallyCenter(table):
             tcVAlign = OxmlElement('w:vAlign')
             tcVAlign.set(qn('w:val'), "center")
             tcPr.append(tcVAlign)
+
+def SetTableWidth_TwoColumns(table_obj, column0_width, column1_width):
+    num_of_col = table_obj._column_count
+    num_of_row = int(len(table_obj._cells) / num_of_col)
+
+    width0 = Inches(column0_width)
+    width1 = Inches(column1_width)
+
+    for i in range(num_of_row):
+        table_obj.cell(i,0).width = width0
+        table_obj.cell(i,1).width = width1
+    return table_obj
+
 
